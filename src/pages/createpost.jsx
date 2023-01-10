6d1146ca602daba3d6fce7378707086045aa7d90
kirran01 <Kirran-@live.com>
2023-01-10 00:19:25 -0400

    create a post page

--- src/pages/createpost.jsx
+++ src/pages/createpost.jsx
@@ -4,13 +4,29 @@ const Createpost = () => {
     return (
         <div className='create-post-page'>
             <div className='create-post-container'>
-                <form className='create-post-form'>
-                    <input placeholder='Add your title' type="text" />
-                    <input placeholder='Tell everyone what your Pin is about' type="text" />
-                    <input placeholder='Add destination link' type="text" />
-                    <input placeholder='URL' type="text" />
-                    <button>Upload</button>
-                </form>
+                <div className='upload-post-container'>
+                    <div className='upload-post'>
+                        <div className='upload-post-icon-container'><img className='upload-post-icon' style={{ height: '35px' }} src="../../public/img/upload.png" alt="upload-icon" /> <h4>drag and drop  or click to <br />upload</h4></div>
+
+                        <h5>Reccomendation: Use high-quality .jpg files less than 20MB</h5>
+                    </div>
+                    <button>Save from site</button>
+                </div>
+                <div className='create-post-form-container'>
+                    <form className='create-post-form'>
+                        <input placeholder='Add your title' type="text" />
+                        <br />
+                        <input placeholder='Tell everyone what your Pin is about' type="text" />
+                        <br />
+                        <input placeholder='Add destination link' type="text" />
+                        <br />
+                        <input placeholder='URL' type="text" />
+                        <br />
+                        <div className='create-post-form-button'>
+                        <button>Upload</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
