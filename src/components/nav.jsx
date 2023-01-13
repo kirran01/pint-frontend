e275b740c87978428b3b6a245dae9f8de8b9a255
kirran01 <Kirran-@live.com>
2023-01-12 20:44:21 -0400

    navbar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -6,6 +6,10 @@ import Signupform from './signupform';
 import { AuthContext } from '../context/auth.context';
 import { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 
 
@@ -37,6 +41,9 @@ const Nav = () => {
     const [modalIsOpen2, setIsOpen2] = useState(false);
     return (
         <nav className='nav'>
+            <div className='pintrest-logo'>
+                <img style={{ height: '32px' }} src="../../public/img/pint.png" alt="img" />
+            </div>
             <ul>
                 <li onClick={openModal}>
                     Sign Up
@@ -54,6 +61,14 @@ const Nav = () => {
                     Create
                 </Link>
             </ul>
+            <div className='searchbar-container'>
+                <span className='searchbar-icon'><img style={{height:'20px'}} src="../../public/img/magnify.png" alt="" /></span>
+            </div>
+            <input id="searchbarId" className='searchbar' type="text" placeholder='Search' />
+            <NotificationsIcon />
+            <ChatBubbleIcon />
+            <AccountCircleIcon />
+            <ExpandMoreIcon />
             <Modal
                 isOpen={modalIsOpen}
                 onRequestClose={closeModal}
